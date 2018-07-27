485d965b1282a59544c6b4016f5271db1da7aa34
Jack Dewinter <jackdewinter1@gmail.com>
2018-07-27 11:45:01 +0100

    set up function for generating list

--- listgen.js
+++ listgen.js
@@ -2,3 +2,9 @@
 // Output is a list of random numbers 
 
 console.log("Random number ", Math.floor((Math.random() * 10) + 1));
+
+function listGen(amount, max){
+    console.log("Generating a list of " + amount + " numbers between 1 and " + max);
+}
+
+listGen(10, 100);
