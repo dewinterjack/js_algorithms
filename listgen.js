2366da0e37cfe865766320a4263c65945b7ddfe1
Jack Dewinter <jackdewinter1@gmail.com>
2018-07-27 11:42:29 +0100

    set up random number gen module

---
+++ listgen.js
@@ -0,0 +1,4 @@
+// Input size of list and range of numbers. e.g. 7 items between 1 and 100
+// Output is a list of random numbers 
+
+console.log("Random number ", Math.floor((Math.random() * 10) + 1));
